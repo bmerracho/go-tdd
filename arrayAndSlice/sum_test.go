f721c2e9923822536c34e989da39380f2a15f871
Buena Mar Erracho <buenamarerracho97@gmail.com>
2022-04-09 01:30:55 +0800

    - refactor convert array into slice, add testcase

--- arrayAndSlice/sum_test.go
+++ arrayAndSlice/sum_test.go
@@ -3,12 +3,25 @@ package arrayAndSlice
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{5, 3, 4, 2, 1}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{5, 3, 4, 2, 1}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q, given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{5, 4, 2, 1}
+
+		got := Sum(numbers)
+		want := 12
+
+		if got != want {
+			t.Errorf("got %q, want %q given %v", got, want, numbers)
+		}
+	})
 }
