09b16c15624e988da60a13501a856c432b5582b9
Buena Mar Erracho <buenamarerracho97@gmail.com>
2022-04-09 01:34:30 +0800

    - removed redundant testcase, refactor fmt

--- arrayAndSlice/sum_test.go
+++ arrayAndSlice/sum_test.go
@@ -10,18 +10,7 @@ func TestSum(t *testing.T) {
 		want := 15
 
 		if got != want {
-			t.Errorf("got %q want %q, given %v", got, want, numbers)
-		}
-	})
-
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{5, 4, 2, 1}
-
-		got := Sum(numbers)
-		want := 12
-
-		if got != want {
-			t.Errorf("got %q, want %q given %v", got, want, numbers)
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
 	})
 }
